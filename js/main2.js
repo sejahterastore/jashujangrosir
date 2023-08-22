36d80aaaac01cac320ffc4801d02e333b27d2e6b
sejahterastore <sejahterastore0@gmail.com>
2023-08-22 12:07:26 +0700

    Initial commit

--- js/main2.js
+++ js/main2.js
@@ -1,10 +1,10 @@
 $(document).on('click', '.send', function() {
     /* Form Input */
-    var input_name = $("#name").val(),
-        input_phone = $("#phone").val(),
-        input_pembayaran = $("#pembayaran").val(),
-        input_warna = $("#warna").val(),
-        input_description = $("#description").val();
+    var input_name = $("#name2").val(),
+        input_phone = $("#phone2").val(),
+        input_pembayaran = $("#pembayaran2").val(),
+        input_warna = $("#warna2").val(),
+        input_description = $("#description2").val();
 
     /* Whatsapp Setting */
     var walink = 'https://web.whatsapp.com/send',
@@ -36,8 +36,10 @@ $(document).on('click', '.send', function() {
 
         /* Whatsapp Window Open */
         window.open(checkout_whatsapp, '_blank');
-        document.getElementById("text-info").innerHTML = '<div class="alert alert-success">' + text_yes + '</div>';
+
+        document.getElementById("text-info2").innerHTML = '<div class="alert alert-success">' + text_yes + '</div>';
     } else {
-        document.getElementById("text-info").innerHTML = '<div class="alert alert-danger">' + text_no + '</div>';
+
+        document.getElementById("text-info2").innerHTML = '<div class="alert alert-danger">' + text_no + '</div>';
     }
 });
